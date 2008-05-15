fe91685137dde122491e37ee00c56c48e9e95c59
Timo Hirvonen <tihirvon@gmail.com>
2008-05-15 22:33:40 +0300

    run: Reorder compiler error message patterns

--- spawn.c
+++ spawn.c
@@ -18,13 +18,13 @@ struct error_pattern {
 };
 
 static const struct error_pattern error_patterns[] = {
+	{  0,  0, -1, -1, "error: \\(Each undeclared identifier is reported only once" },
+	{  0,  0, -1, -1, "error: for each function it appears in.\\)" },
 	{  3,  3,  1,  2, "^(.+):([0-9]+): (.*)" },
 	{  3,  0,  1,  2, "^.* at (.+):([0-9]+):$" },
 	{  2,  0,  1,  2, "^In file included from (.+):([0-9]+)[,:]" },
 	{  2,  0,  1,  2, "^ +from (.+):([0-9]+)[,:]" },
 	{  2,  2,  1, -1, "^(.+): (In function .*:)$" },
-	{  0,  0, -1, -1, "^error: \\(Each undeclared identifier is reported only once" },
-	{  0,  0, -1, -1, "^error: for each function it appears in.\\)" },
 	{  1,  0, -1, -1, "^make: \\*\\*\\* \\[.*\\] Error [0-9]+$" },
 	{  1,  0, -1, -1, "^collect2: ld returned [0-9]+ exit status$" },
 	{ -1, -1, -1, -1, NULL }
