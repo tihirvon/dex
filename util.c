ccacb0ce5567e0825f1cb722d6f7b8cf817aa665
Timo Hirvonen <tihirvon@gmail.com>
2009-02-21 15:36:37 +0200

    Dump core on bug
    This is probably the best way to get backtrace.

--- util.c
+++ util.c
@@ -123,7 +123,9 @@ void bug(const char *function, const char *fmt, ...)
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
 	va_end(ap);
-	exit(42);
+
+	// for core dump
+	abort();
 }
 
 void debug_print(const char *function, const char *fmt, ...)
