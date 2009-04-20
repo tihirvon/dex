97a9dd1c47b95ecb08643097f94805f6b876eeec
Timo Hirvonen <tihirvon@gmail.com>
2009-04-20 19:22:45 +0300

    Use ptr_array to store highlight colors

--- color.c
+++ color.c
@@ -1,32 +1,26 @@
 #include "common.h"
 #include "color.h"
+#include "ptr-array.h"
 
-static struct hl_color **hl_colors;
-static int color_count;
-static int color_alloc;
+static PTR_ARRAY(hl_colors);
 
 struct hl_color *set_highlight_color(const char *name, const struct term_color *color)
 {
 	struct hl_color *c;
 	int i;
 
-	for (i = 0; i < color_count; i++) {
-		c = hl_colors[i];
+	for (i = 0; i < hl_colors.count; i++) {
+		c = hl_colors.ptrs[i];
 		if (!strcmp(name, c->name)) {
 			c->color = *color;
 			return c;
 		}
 	}
 
-	if (color_count == color_alloc) {
-		color_alloc = color_alloc * 3 / 2;
-		color_alloc = (color_alloc + 4) & ~3;
-		xrenew(hl_colors, color_alloc);
-	}
 	c = xnew(struct hl_color, 1);
 	c->name = xstrdup(name);
 	c->color = *color;
-	hl_colors[color_count++] = c;
+	ptr_array_add(&hl_colors, c);
 	return c;
 }
 
@@ -34,9 +28,10 @@ static struct hl_color *find_real_color(const char *name)
 {
 	int i;
 
-	for (i = 0; i < color_count; i++) {
-		if (!strcmp(hl_colors[i]->name, name))
-			return hl_colors[i];
+	for (i = 0; i < hl_colors.count; i++) {
+		struct hl_color *c = hl_colors.ptrs[i];
+		if (!strcmp(c->name, name))
+			return c;
 	}
 	return NULL;
 }
