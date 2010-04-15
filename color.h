b42f436637201dba6e69a15c6df5a3c6cd8bba4b
Timo Hirvonen <tihirvon@gmail.com>
2010-04-15 20:49:13 +0300

    Move parse_term_color() to color.c

--- color.h
+++ color.h
@@ -10,5 +10,6 @@ struct hl_color {
 
 struct hl_color *set_highlight_color(const char *name, const struct term_color *color);
 struct hl_color *find_color(const char *name);
+int parse_term_color(struct term_color *color, char **strs);
 
 #endif
