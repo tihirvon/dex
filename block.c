8216cc620e8400162847020d6f5afe836c4358ea
Timo Hirvonen <tihirvon@gmail.com>
2010-04-23 02:33:45 +0300

    Split large blocks into smaller ones on insert

--- block.c
+++ block.c
@@ -55,74 +55,150 @@ static unsigned int insert_to_current(const char *buf, unsigned int len)
 	return nl;
 }
 
-static unsigned int insert_to_next(const char *buf, unsigned int len)
-{
-	BUG_ON(view->cursor.offset != view->cursor.blk->size);
-	BUG_ON(view->cursor.blk->node.next == &buffer->blocks);
-	view->cursor.blk = BLOCK(view->cursor.blk->node.next);
-	view->cursor.offset = 0;
-	return insert_to_current(buf, len);
-}
-
-static unsigned int append_to_current(const char *buf, unsigned int len)
+/*
+ * Combine current block and new data into smaller blocks:
+ *   - Block _must_ contain whole lines
+ *   - Block _must_ contain at least one line
+ *   - Preferred maximum size of block is BLOCK_EDIT_SIZE
+ *   - Size of any block can be larger than BLOCK_EDIT_SIZE
+ *     only if there's a very long line
+ */
+static unsigned int split_and_insert(const char *buf, unsigned int len)
 {
 	struct block *blk = view->cursor.blk;
-	unsigned int size = blk->size + len;
-	unsigned int nl;
+	struct list_head *prev_node = blk->node.prev;
+	const char *buf1 = blk->data;
+	const char *buf2 = buf;
+	const char *buf3 = blk->data + view->cursor.offset;
+	unsigned int size1 = view->cursor.offset;
+	unsigned int size2 = len;
+	unsigned int size3 = blk->size - size1;
+	unsigned int total = size1 + size2 + size3;
+	unsigned int start = 0; // beginning of new block
+	unsigned int size = 0;  // size of new block
+	unsigned int pos = 0;   // current position
+	unsigned int nl_added = 0;
 
-	if (size > blk->alloc) {
-		blk->alloc = ALLOC_ROUND(size);
-		xrenew(blk->data, blk->alloc);
+	while (start < total) {
+		// size of new block if next line would be added
+		unsigned int new_size = 0;
+		unsigned int copied = 0;
+		struct block *new;
+
+		if (pos < size1) {
+			const char *nl = memchr(buf1 + pos, '\n', size1 - pos);
+			if (nl)
+				new_size = nl - buf1 + 1 - start;
+		}
+
+		if (!new_size && pos < size1 + size2) {
+			unsigned int offset = 0;
+			const char *nl;
+
+			if (pos > size1)
+				offset = pos - size1;
+
+			nl = memchr(buf2 + offset, '\n', size2 - offset);
+			if (nl)
+				new_size = size1 + nl - buf2 + 1 - start;
+		}
+
+		if (!new_size && pos < total) {
+			unsigned int offset = 0;
+			const char *nl;
+
+			if (pos > size1 + size2)
+				offset = pos - size1 - size2;
+
+			nl = memchr(buf3 + offset, '\n', size3 - offset);
+			if (nl)
+				new_size = size1 + size2 + nl - buf3 + 1 - start;
+			else
+				new_size = total - start;
+		}
+
+		if (new_size <= BLOCK_EDIT_SIZE) {
+			// fits
+			size = new_size;
+			pos = start + new_size;
+			if (pos < total)
+				continue;
+		} else {
+			// does not fit
+			if (!size) {
+				// one block containing one very long line
+				size = new_size;
+				pos = start + new_size;
+			}
+		}
+
+		BUG_ON(!size);
+		new = block_new(size);
+		if (start < size1) {
+			unsigned int avail = size1 - start;
+			unsigned int count = size;
+
+			if (count > avail)
+				count = avail;
+			new->nl += copy_count_nl(new->data, buf1 + start, count);
+			copied += count;
+			start += count;
+		}
+		if (start >= size1 && start < size1 + size2) {
+			unsigned int offset = start - size1;
+			unsigned int avail = size2 - offset;
+			unsigned int count = size - copied;
+
+			if (count > avail)
+				count = avail;
+			new->nl += copy_count_nl(new->data + copied, buf2 + offset, count);
+			copied += count;
+			start += count;
+		}
+		if (start >= size1 + size2) {
+			unsigned int offset = start - size1 - size2;
+			unsigned int avail = size3 - offset;
+			unsigned int count = size - copied;
+
+			BUG_ON(count > avail);
+			new->nl += copy_count_nl(new->data + copied, buf3 + offset, count);
+			copied += count;
+			start += count;
+		}
+
+		new->size = size;
+		BUG_ON(copied != size);
+		list_add_before(&new->node, &blk->node);
+
+		nl_added += new->nl;
 	}
-	nl = copy_count_nl(blk->data + blk->size, buf, len);
-	blk->nl += nl;
-	blk->size = size;
-	return nl;
-}
 
-static unsigned int add_new_block(const char *buf, unsigned int len)
-{
-	struct block *blk = block_new(len);
+	view->cursor.blk = BLOCK(prev_node->next);
+	while (view->cursor.offset > view->cursor.blk->size) {
+		view->cursor.offset -= view->cursor.blk->size;
+		view->cursor.blk = BLOCK(view->cursor.blk->node.next);
+	}
 
-	blk->nl = copy_count_nl(blk->data, buf, len);
-	blk->size = len;
-	list_add_after(&blk->node, &view->cursor.blk->node);
-	return blk->nl;
+	nl_added -= blk->nl;
+	delete_block(blk);
+	return nl_added;
 }
 
 static unsigned int insert_bytes(const char *buf, unsigned int len)
 {
 	struct block *blk = view->cursor.blk;
-	struct block *next = NULL;
-	unsigned int offset = view->cursor.offset;
+	unsigned int new_size = blk->size + len;
 
-	if (offset < blk->size)
+	// never append in case last character of buf is not newline
+	if (view->cursor.offset == blk->size && blk->node.next != &buffer->blocks) {
+		blk = BLOCK(blk->node.next);
+		view->cursor.blk = blk;
+		view->cursor.offset = 0;
+	}
+
+	if (new_size <= blk->alloc || new_size <= BLOCK_EDIT_SIZE)
 		return insert_to_current(buf, len);
-
-	if (!blk->size || blk->data[blk->size - 1] != '\n') {
-		// must append to this block
-		return append_to_current(buf, len);
-	}
-
-	if (blk->node.next != &buffer->blocks)
-		next = BLOCK(blk->node.next);
-
-	if (buf[len - 1] != '\n' && next) {
-		// must insert to beginning of next block
-		return insert_to_next(buf, len);
-	}
-
-	if (blk->size + len > BLOCK_EDIT_SIZE) {
-		// this block would grow too big, insert to next or add new?
-		if (next && len + next->size <= BLOCK_EDIT_SIZE) {
-			// fits to next block
-			return insert_to_next(buf, len);
-		}
-		return add_new_block(buf, len);
-	}
-
-	// fits to this block
-	return append_to_current(buf, len);
+	return split_and_insert(buf, len);
 }
 
 void do_insert(const char *buf, unsigned int len)
