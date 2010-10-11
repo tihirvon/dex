bba3a135805a1856cd7404adfbbdd9740c6e0f39
Timo Hirvonen <tihirvon@gmail.com>
2010-10-11 21:21:10 +0300

    Remove ~ and .bak etc. from end of filename before finding extension

--- filetype.c
+++ filetype.c
@@ -11,6 +11,11 @@ struct filetype {
 
 static PTR_ARRAY(filetypes);
 
+static const char *ignore[] = {
+	"bak", "dpkg-dist", "dpkg-old", "new", "old", "orig", "pacnew",
+	"pacorig", "pacsave", "rpmnew", "rpmsave",
+};
+
 void add_filetype(const char *name, const char *str, enum detect_type type)
 {
 	struct filetype *ft;
@@ -22,17 +27,53 @@ void add_filetype(const char *name, const char *str, enum detect_type type)
 	ptr_array_add(&filetypes, ft);
 }
 
+// file.c.old~ -> c
+// file..old   -> old
+// file.old    -> old
+static char *get_ext(const char *filename)
+{
+	const char *ext = strrchr(filename, '.');
+	int ext_len, i;
+
+	if (!ext)
+		return NULL;
+
+	ext++;
+	ext_len = strlen(ext);
+	if (ext_len && ext[ext_len - 1] == '~')
+		ext_len--;
+	if (!ext_len)
+		return NULL;
+
+	for (i = 0; i < ARRAY_COUNT(ignore); i++) {
+		if (!strncmp(ignore[i], ext, ext_len) && !ignore[i][ext_len]) {
+			int idx = -2;
+			while (ext + idx >= filename) {
+				if (ext[idx] == '.') {
+					int len = -idx - 2;
+					if (len) {
+						ext -= len + 1;
+						ext_len = len;
+					}
+					break;
+				}
+				idx--;
+			}
+			break;
+		}
+	}
+	return xstrndup(ext, ext_len);
+}
+
 const char *find_ft(const char *filename, const char *interpreter,
 	const char *first_line, unsigned int line_len)
 {
 	unsigned int filename_len = strlen(filename);
-	const char *ext = NULL;
+	char *ext = NULL;
 	int i;
 
 	if (filename)
-		ext = strrchr(filename, '.');
-	if (ext)
-		ext++;
+		ext = get_ext(filename);
 	for (i = 0; i < filetypes.count; i++) {
 		const struct filetype *ft = filetypes.ptrs[i];
 
@@ -54,8 +95,10 @@ const char *find_ft(const char *filename, const char *interpreter,
 				continue;
 			break;
 		}
+		free(ext);
 		return ft->name;
 	}
+	free(ext);
 	return NULL;
 }
 
