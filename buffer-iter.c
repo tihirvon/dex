1810754fc2d92870b4de999b223ab684fa0c3cc5
Timo Hirvonen <tihirvon@gmail.com>
2010-11-05 14:51:53 +0200

    Small cleanup to buffer-iter.c

--- buffer-iter.c
+++ buffer-iter.c
@@ -8,46 +8,44 @@ unsigned int buffer_get_char(struct block_iter *bi, uchar *up)
 
 unsigned int buffer_next_char(struct block_iter *bi, uchar *up)
 {
-	struct block *blk = bi->blk;
 	unsigned int offset = bi->offset;
 
-	if (offset == blk->size) {
-		if (blk->node.next == bi->head)
+	if (offset == bi->blk->size) {
+		if (bi->blk->node.next == bi->head)
 			return 0;
-		bi->blk = blk = BLOCK(blk->node.next);
+		bi->blk = BLOCK(bi->blk->node.next);
 		bi->offset = offset = 0;
 	}
 
 	// Note: this block can't be empty
-	*up = ((unsigned char *)blk->data)[offset];
+	*up = ((unsigned char *)bi->blk->data)[offset];
 	if (*up < 0x80 || !buffer->options.utf8) {
 		bi->offset++;
 		return 1;
 	}
 
-	*up = u_buf_get_char(blk->data, blk->size, &bi->offset);
+	*up = u_buf_get_char(bi->blk->data, bi->blk->size, &bi->offset);
 	return bi->offset - offset;
 }
 
 unsigned int buffer_prev_char(struct block_iter *bi, uchar *up)
 {
-	struct block *blk = bi->blk;
 	unsigned int offset = bi->offset;
 
 	if (!offset) {
-		if (blk->node.prev == bi->head)
+		if (bi->blk->node.prev == bi->head)
 			return 0;
-		bi->blk = blk = BLOCK(blk->node.prev);
-		bi->offset = offset = blk->size;
+		bi->blk = BLOCK(bi->blk->node.prev);
+		bi->offset = offset = bi->blk->size;
 	}
 
 	// Note: this block can't be empty
-	*up = ((unsigned char *)blk->data)[offset - 1];
+	*up = ((unsigned char *)bi->blk->data)[offset - 1];
 	if (*up < 0x80 || !buffer->options.utf8) {
 		bi->offset--;
 		return 1;
 	}
 
-	*up = u_prev_char(blk->data, &bi->offset);
+	*up = u_prev_char(bi->blk->data, &bi->offset);
 	return offset - bi->offset;
 }
