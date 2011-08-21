ee5d6248ce7b75834e872ac79896c9b9c1a4d248
Timo Hirvonen <tihirvon@gmail.com>
2011-08-21 20:22:32 +0300

    Move all possible inline functions from uchar.h to uchar.c
    Private low-level stuff and not needed anywhere else.

--- uchar.h
+++ uchar.h
@@ -18,48 +18,6 @@ static inline unsigned int u_char_size(unsigned int uch)
 	return 1;
 }
 
-static inline int u_seq_len(unsigned int first_byte)
-{
-	if (first_byte < 0x80)
-		return 1;
-	if (first_byte < 0xc0)
-		return 0;
-	if (first_byte < 0xe0)
-		return 2;
-	if (first_byte < 0xf0)
-		return 3;
-
-	// could be 0xf8 but RFC 3629 doesn't allow codepoints above 0x10ffff
-	if (first_byte < 0xf5)
-		return 4;
-	return -1;
-}
-
-static inline int u_is_continuation(unsigned int uch)
-{
-	return (uch & 0xc0) == 0x80;
-}
-
-static inline int u_seq_len_ok(unsigned int uch, int len)
-{
-	return u_char_size(uch) == len;
-}
-
-/*
- * Len  Mask         Note
- * -------------------------------------------------
- * 1    0111 1111    Not supported by this function!
- * 2    0001 1111
- * 3    0000 1111
- * 4    0000 0111
- * 5    0000 0011    Forbidded by RFC 3629
- * 6    0000 0001    Forbidded by RFC 3629
- */
-static inline unsigned int u_get_first_byte_mask(unsigned int len)
-{
-	return (1U << 7U >> len) - 1U;
-}
-
 static inline void u_set_ctrl(char *buf, unsigned int *idx, unsigned int u)
 {
 	unsigned int i = *idx;
